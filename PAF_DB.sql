a0bc31ba20992afdc1f608338c11d166e39dc9ae
chanuri de silva <53204670+chanu16@users.noreply.github.com>
2020-05-06 20:51:42 +0530

    New version of Database added

---
+++ PAF_DB.sql
@@ -0,0 +1,313 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 06, 2020 at 05:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pafdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `adminID` int(10) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `username` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminID`, `name`, `password`, `username`) VALUES
+(1, 'John', 'John123', 'john@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment`
+--
+
+CREATE TABLE `appointment` (
+  `appointmentID` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `hospitalID` int(11) NOT NULL,
+  `patientID` int(11) NOT NULL,
+  `doctorID` int(11) NOT NULL,
+  `refundID` int(11) NOT NULL,
+  `paymentID` int(11) NOT NULL,
+  `appointmentStatus` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doctorID` int(11) NOT NULL,
+  `NIC` varchar(13) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `specification` varchar(100) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `workDate` varchar(10) NOT NULL,
+  `workTime` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `adminID` int(10) NOT NULL,
+  `doctorStatus` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`doctorID`, `NIC`, `gender`, `firstName`, `lastName`, `email`, `specification`, `contact`, `workDate`, `workTime`, `password`, `adminID`, `doctorStatus`) VALUES
+(32, '996756756V', 'female', 'Sandali', 'Pamoda', 'sandali@gmail.com', 'cardio', '0778976549', 'Wednesday', 'Evening', 'sanda123', 1, 'true'),
+(41, '986754675V', 'female', 'jane', 'smith', 'jane@gmail.com', 'neuro', '0776543456', 'monday', 'eve', '123', 1, 'false');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hospital`
+--
+
+CREATE TABLE `hospital` (
+  `hospitalID` int(50) NOT NULL,
+  `hospitalName` varchar(50) NOT NULL,
+  `adminID` int(11) NOT NULL,
+  `location` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patientID` int(11) NOT NULL,
+  `NIC` varchar(13) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `contact` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `paymentID` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `dateAndTime` datetime NOT NULL DEFAULT current_timestamp(),
+  `amount` double NOT NULL,
+  `paymentStatus` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `refun`
+--
+
+CREATE TABLE `refun` (
+  `refunID` int(11) NOT NULL,
+  `amount` double NOT NULL,
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `method` int(11) NOT NULL,
+  `adminID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `worksin`
+--
+
+CREATE TABLE `worksin` (
+  `hospitalID` int(11) NOT NULL,
+  `doctorID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`adminID`);
+
+--
+-- Indexes for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD PRIMARY KEY (`appointmentID`),
+  ADD KEY `hospitalID` (`hospitalID`),
+  ADD KEY `patientID` (`patientID`),
+  ADD KEY `doctorID` (`doctorID`),
+  ADD KEY `refundID` (`refundID`),
+  ADD KEY `paymentID` (`paymentID`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doctorID`),
+  ADD KEY `adminID` (`adminID`);
+
+--
+-- Indexes for table `hospital`
+--
+ALTER TABLE `hospital`
+  ADD PRIMARY KEY (`hospitalID`),
+  ADD KEY `adminID` (`adminID`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patientID`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`paymentID`);
+
+--
+-- Indexes for table `refun`
+--
+ALTER TABLE `refun`
+  ADD PRIMARY KEY (`refunID`),
+  ADD KEY `adminID` (`adminID`);
+
+--
+-- Indexes for table `worksin`
+--
+ALTER TABLE `worksin`
+  ADD PRIMARY KEY (`hospitalID`,`doctorID`),
+  ADD KEY `hospitalID` (`hospitalID`),
+  ADD KEY `doctorID` (`doctorID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `adminID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `appointment`
+--
+ALTER TABLE `appointment`
+  MODIFY `appointmentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `doctorID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT for table `hospital`
+--
+ALTER TABLE `hospital`
+  MODIFY `hospitalID` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patient`
+--
+ALTER TABLE `patient`
+  MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `paymentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `refun`
+--
+ALTER TABLE `refun`
+  MODIFY `refunID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `fk1` FOREIGN KEY (`hospitalID`) REFERENCES `hospital` (`hospitalID`),
+  ADD CONSTRAINT `fk2` FOREIGN KEY (`patientID`) REFERENCES `patient` (`patientID`),
+  ADD CONSTRAINT `fk3` FOREIGN KEY (`doctorID`) REFERENCES `doctor` (`doctorID`),
+  ADD CONSTRAINT `fk4` FOREIGN KEY (`refundID`) REFERENCES `refun` (`refunID`),
+  ADD CONSTRAINT `fk5` FOREIGN KEY (`paymentID`) REFERENCES `payment` (`paymentID`);
+
+--
+-- Constraints for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD CONSTRAINT `d1` FOREIGN KEY (`adminID`) REFERENCES `admin` (`adminID`);
+
+--
+-- Constraints for table `hospital`
+--
+ALTER TABLE `hospital`
+  ADD CONSTRAINT `h1` FOREIGN KEY (`adminID`) REFERENCES `admin` (`adminID`);
+
+--
+-- Constraints for table `refun`
+--
+ALTER TABLE `refun`
+  ADD CONSTRAINT `r1` FOREIGN KEY (`adminID`) REFERENCES `admin` (`adminID`);
+
+--
+-- Constraints for table `worksin`
+--
+ALTER TABLE `worksin`
+  ADD CONSTRAINT `w1` FOREIGN KEY (`hospitalID`) REFERENCES `hospital` (`hospitalID`),
+  ADD CONSTRAINT `w2` FOREIGN KEY (`doctorID`) REFERENCES `doctor` (`doctorID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
